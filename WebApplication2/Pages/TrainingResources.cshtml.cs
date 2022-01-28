c577fb96246b76bed1b82dbdd3d404e363662fe4
Weiguang <44439424+xiaoweiguang@users.noreply.github.com>
2022-01-28 14:04:44 -0500

    initial commit

---
+++ WebApplication2/Pages/TrainingResources.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages
+{
+    public class TrainingResourcesModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TrainingResourcesModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
